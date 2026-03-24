57f35f5d631166664f9fbf70f45b5c7e5d70b7a5
Pet-Garcia <petrusgarcia11@gmail.com>
2026-03-24 00:08:32 +0000

    Aula 23/03/2026

---
+++ fundamentos_de_dart/bin/media.dart
@@ -0,0 +1,17 @@
+import 'dart.io';
+
+void main() {
+  // Verifica e depois converte
+  stdout.write('Qual a nota da prova? ');
+  String? valor1 = stdin.readLineSync();
+  // ?? 0 -> se não conseguir nada, coloca 0 / tenta fazer o double.tryParse(valor1!) se não coloca 0
+  double? prova = double.tryParse(valor1!) ?? 0;
+
+  // Converte direto (não recomendado)
+  stdout.write('Qual a nota do trabalho? ');
+  double? trabalho = double.tryParse(stdin.readLineSync()!) ?? 0;
+
+  double? media = prova * 0.6 + trabalho * 0.4;
+
+  stdout.write('A sua média é $media \n');
+}
