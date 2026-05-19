d5a8b9a36ddc870b86bd13f0957b390a7d840427
Pet-Garcia <petrusgarcia11@gmail.com>
2026-05-18 21:04:41 -0300

    Trabalhado nos exercicios do quarto trabalho parcial
    Trabalho feito em flutter

---
+++ quarto_trabalho_parcial/lib/Ex5_tela_configuracao.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+class TelaConfiguracaoApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Configuração',
+      home: TelaConfiguracaoPage(),
+    );
+  }
+}
+
+class TelaConfiguracaoPage extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return TelaConfiguracaoState();
+  }
+}
+
+bool _notificacaoAtiva = false;
+bool _modoEscuroAtivo = false;
+
+class TelaConfiguracaoState extends State<TelaConfiguracaoPage>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Configuração'),
+      ),
+
+      body: Padding(
+        padding: EdgeInsetsGeometry.all(25),
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Qual o seu Nome?',
+              ),
+
+            ),
+
+            SizedBox(
+              height: 50,
+            ),
+
+            SwitchListTile(
+              title: Text('Ativar Notificações'),
+              value: _notificacaoAtiva,
+              onChanged: (bool novovalor){
+                setState((){
+                  _notificacaoAtiva = novovalor;
+                });
+              }
+            ),
+
+            SwitchListTile(
+              title: Text('Modo Escuro'),
+              value: _modoEscuroAtivo,
+              onChanged: (bool novovalor){
+                setState((){
+                  _modoEscuroAtivo = novovalor;
+                });
+              }
+            )
+
+          ]
+        )
+      )
+    );
+  }
+}
