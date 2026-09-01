5e8bc7d4febb4594e68721597be5f873f6ed9fd3
Pet-Garcia <petrusgarcia11@gmail.com>
2026-08-31 21:53:21 -0300

    Delete primeiro_trabalho_parcial directory

--- primeiro_trabalho_parcial/test/primeiro_trabalho_parcial_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:primeiro_trabalho_parcial/primeiro_trabalho_parcial.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
